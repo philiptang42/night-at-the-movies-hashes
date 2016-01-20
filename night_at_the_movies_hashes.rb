7f91334b4756048e789f6f3950467014fd8b1bcd
philiptang42 <philiptang42@gmail.com>
2016-01-19 20:55:28 -0500

    add case added

--- night_at_the_movies_hashes.rb
+++ night_at_the_movies_hashes.rb
@@ -2,12 +2,16 @@ movies = {
   star_wars_vii: 4
 }
 
-puts "Add a movie"
+puts "Input add, update, display or delete"
 choice = gets.chomp
 
 case choice
   when "add"
-    puts "Added!"
+    puts "Input a movie title"
+    title = gets.chomp
+    puts "Input its rating"
+    rating = gets.chomp
+    movies["#{title}"] = "#{rating}"
   when "update"
     puts "Updated!"
   when "display"
