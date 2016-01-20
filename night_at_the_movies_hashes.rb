091585815ed9bcac44979ea63389b04c540f71a1
philiptang42 <philiptang42@gmail.com>
2016-01-19 20:18:11 -0500

    using case

--- night_at_the_movies_hashes.rb
+++ night_at_the_movies_hashes.rb
@@ -1,6 +1,19 @@
 movies = {
-    star_wars_vii: 4
+  star_wars_vii: 4
 }
 
 puts "Add a movie"
 choice = gets.chomp
+
+case choice
+  when "add"
+    puts "Added!"
+  when "update"
+    puts "Updated!"
+  when "display"
+    puts "Movies!"
+  when "delete"
+    puts "Deleted!"
+  else
+    puts "Error!"
+end
