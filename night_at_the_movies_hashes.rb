22ce62b1396a0ed1980b745061c0f555d8aacb08
philiptang42 <philiptang42@gmail.com>
2016-01-19 21:24:56 -0500

    added nil condition to title

--- night_at_the_movies_hashes.rb
+++ night_at_the_movies_hashes.rb
@@ -9,9 +9,13 @@ case choice
   when "add"
     puts "Input a movie title"
       title = gets.chomp.to_sym
-    puts "Input its rating"
+    if movies[title].nil?
+      puts "Input its rating"
       rating = gets.chomp.to_i
-    movies[title] = rating
+      movies[title] = rating
+    else
+      puts "That movie is already in the list"
+    end
   when "update"
     puts "Updated!"
   when "display"
